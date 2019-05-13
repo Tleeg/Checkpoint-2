5733a349fe9d14c3bb37a973fd845a6a6c1f0740
unknown <tleeg213@gmail.com>
2019-05-12 20:26:43 -0600

    done

---
+++ pageObjects/checkObjects.js
@@ -0,0 +1,17 @@
+module.exports = {
+    url: 'https://devmountain-qa.github.io/Automation-Basics/build/',
+    elements: {
+        num: '[name="evenOddInput"]',
+        confirmnum: '[name="evenOddButton"]',
+        obj: '[name="objectFilterInput"]',
+        confirmobj: '[name="objectFilterButton"]',
+        string: '#nameFilterInput',
+        confirmstring: '#nameFilterButton',
+        palin: '[name="palindromeInput"]',
+        confirmpalin: '[name="palindromeButton"]',
+        sum1: '[name="sumInput1"]',
+        sum2: '[name="sumInput2"]',
+        confirmsum: '[name="sumButton"]'
+        
+    }
+}
--- tests/test.js
+++ tests/test.js
@@ -1,11 +1,44 @@
+var checkObjects
 module.exports = {
     beforeEach: browser => {
-        browser.url('https://devmountain-qa.github.io/Automation-Basics/build/')
+        checkObjects = browser.page.checkObjects()
+        checkObjects.navigate()
     },
     after: browser => {
         browser.end()
     },
-    'test 1': browser => {
-        browser
+    'Evens and Odds': browser => {
+        checkObjects
+        .setValue('@num', '1,2,3.5,4.5,5.2,6.2')
+        .click('@confirmnum')
+        .verify.containsText('[name="evenResults"]', '2,5.2,6.2')
+        .verify.containsText('[name="oddResults"]', '1,3.5,4.5')
+    },
+    'Filter Object': browser => {
+        checkObjects
+        .setValue('@obj', 'title')
+        .click('@confirmobj')
+        .expect.element('[name="objectFilterResults"]').text.to.equal('Filtered: [ { "name": "Jimmy Joe", "title": "Hack0r", "age": 12 }, { "name": "Carly Armstrong", "title": "CEO" } ]')
+
+    },
+    'Filter String': browser => {
+        checkObjects
+        .setValue('@string', 'M')
+        .click('@confirmstring')
+        .expect.element('[name="nameFilterResults"]').text.to.equal('Filtered Names: [ "Melody", "Mark", "Maddy" ]')
+
+    },
+    'Palindrome': browser => {
+        checkObjects
+        .setValue('@palin', 'racecar')
+        .click('@confirmpalin')
+        .expect.element('[name="palindromeResults"]').text.to.equal('Palindrome: true')
+    },
+    'Sum': browser => {
+        checkObjects
+        .setValue('@sum1', '1')
+        .setValue('@sum2', '4')
+        .click('@confirmsum')
+        .expect.element('[name="sumResults"]').text.to.equal('Sum: 5')
     }
 }
